c9e83d2aabe0f0c3961baedd1019a2af7f84993c
Onur YILDIZ <41267854+onur-yildiz@users.noreply.github.com>
2022-06-10 21:04:45 +0300

    feat: add reconnecting backdrop on dc

--- src/screens/ScrumScreen.tsx
+++ src/screens/ScrumScreen.tsx
@@ -1,8 +1,15 @@
+import {
+  Backdrop,
+  Box,
+  CircularProgress,
+  IconButton,
+  Typography,
+} from "@mui/material";
 import { Outlet, useNavigate, useParams } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../hooks";
 import { useContext, useEffect, useState } from "react";
 
-import { Box } from "@mui/material";
+import ExitToApp from "@mui/icons-material/ExitToApp";
 import HubContext from "../store/hubContext";
 import MemberList from "../components/MemberList";
 import NavBar from "../components/NavBar";
@@ -17,6 +24,11 @@ const MainScreen = () => {
   const loadedRoomId = useAppSelector((state) => state.scrum.room.id);
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
+
+  const handleSignOut = () => {
+    dispatch(leaveRoom(hub.connection));
+    navigate("/");
+  };
 
   useEffect(() => {
     // if roomId is not a valid UUID, redirect to home
@@ -36,6 +48,26 @@ const MainScreen = () => {
       className="full-width"
       sx={{ height: "100%", display: isValidated ? "initial" : "none" }}
     >
+      <Backdrop
+        sx={{
+          color: "#fff",
+          zIndex: 99999999,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          backdropFilter: "blur(5px)",
+        }}
+        open={!hub.isConnected}
+      >
+        <CircularProgress color="inherit" />
+        <Typography sx={{ mt: 2 }}>
+          Connection lost. Trying to reconnect...
+        </Typography>
+        <IconButton color="error" size="large" onClick={handleSignOut}>
+          <ExitToApp />
+        </IconButton>
+      </Backdrop>
       <NavBar />
       <Resizable className="scrum-screen-layout">
         <MemberList />
